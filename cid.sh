604ad111df5057bd638cf73ef28699cc95cc2644
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-03-29 20:40:45 +0200

    Fix bug

--- cid.sh
+++ cid.sh
@@ -7,7 +7,7 @@ make clean install
 # Verify
 next-prayer --version
 # Test
-docker container run -e RUN_LOCAL=true -v $(PWD):/tmp/lint github/super-linter
+docker container run -e RUN_LOCAL=true -v "$(PWD)":/tmp/lint github/super-linter
 # Create docker image
 docker build -t next-prayer .
 docker tag next-prayer abdeltwabmf/next-prayer:2.0.0
